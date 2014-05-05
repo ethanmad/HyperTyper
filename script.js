53ce6764954f28d80f68e122de26dcc00c4c30d4
Ethan Madison <ethanmadison@gmail.com>
2014-05-05 10:49:08 -0700

    Changed functions to variables
    Unnecessary functions like getText() and numWords() were changed to variables text and numWords. Also added comments.

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-getText = function(){ return document.getElementsByClassName('nonHideableWords unselectable')[0].textContent; };
-numWords = function(){ return getText().split(" ").length; };
+//get passage from HTML
+var text = document.getElementsByClassName('nonHideableWords unselectable')[0].textContent;
+
+
+//get number of words in 
+var numWords = getText().split(" ").length;
 
 var textBox = document.getElementsByClassName('txtInput')[0];
-i = 0;
+i = 0; //word counter
 
-function addWord(){
-  word = getText().split(" ")[i++] + " ";
-  textBox.value = word;
+//put words into textBox
+function addWord() {
+  word = text.split(" ")[i++] + " "; //split text into words
+  textBox.value = word; //insert word
 }
 
 addWord();
 
+//key listener for spacebar
 window.onkeyup = function(e) {
   var key = e.keyCode ? e.keyCode : e.which;
   if (key == 32) {
