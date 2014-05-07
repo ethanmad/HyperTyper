754f000ee4b8fa610334e0277018ff4164192a71
Ethan Madison <ethanmadison@gmail.com>
2014-05-07 09:57:46 -0700

    Modularized HyperTyper
    HyperTyper now works on any typing website, provided that it has the text in HTML. HyperTyper just needs the class names of the text and the textbox to input words into.

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
 //index 0 = TypeRacer, 1 = typrX
-var text = ['nonHideableWords unselectable', 'cw-QuotePanel-textToTypePanel'];
-var textBoxes = ['txtInput', 'cw-TypedTextBox race-go'];   
+var textOptions = ['nonHideableWords unselectable', 'cw-QuotePanel-textToTypePanel'];
+var textBoxOptions = ['txtInput', 'cw-TypedTextBox race-go'];   
 
 var website = -1;
-
-//get passage from HTML
-var text = document.getElementsByClassName(text[website])[0].textContent;
-
-
-//get number of words in 
-var numWords = text.split(" ").length;
-
-var textBox = document.getElementsByClassName('txtInput')[0];
-i = 0; //word counter
-
-//put words into textBox
-function addWord() {
-  word = text.split(" ")[i++]; //split text into words
-  textBox.value = word; //insert word
+for(w = 0; w < textOptions.length; w++) {
+  if(!(document.getElementsByClassName(textOptions[w])[0] == null)) {
+    website = w;
+    break;
+  }
 }
 
-addWord();
-
-//key listener for spacebar
-window.onkeyup = function(e) {
-  var key = e.keyCode ? e.keyCode : e.which;
-  if (key == 32) {
-    addWord();
+if(website > -1) {
+  //get passage from HTML
+  var text = document.getElementsByClassName(textOptions[website])[0].textContent;
+  
+  
+  //get number of words in 
+  var numWords = text.split(" ").length;
+  
+  var textBox = document.getElementsByClassName(textBoxOptions[website])[0];
+  i = 0; //word counter
+  
+  //put words into textBox
+  function addWord() {
+    if(i>=numWords) return;
+    word = text.split(" ")[i++]; //split text into words
+    textBox.value = word; //insert word
   }
-};
+  
+  addWord();
+  
+  //key listener for spacebar
+  window.onkeyup = function(e) {
+    var key = e.keyCode ? e.keyCode : e.which;
+    if (key == 32) {
+      addWord();
+    }
+  };
+}
