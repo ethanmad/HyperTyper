8ee2a31b1f68194009112d243d601a43d45e4e6a
Ethan Madison <ethanmadison@gmail.com>
2014-05-07 09:17:16 -0700

    Added support for typrx.com, minor fixes

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+//index 0 = TypeRacer, 1 = typrX
+var text = ['nonHideableWords unselectable', 'cw-QuotePanel-textToTypePanel'];
+var textBoxes = ['txtInput', 'cw-TypedTextBox race-go'];   
+
+var website = -1;
+
 //get passage from HTML
-var text = document.getElementsByClassName('nonHideableWords unselectable')[0].textContent;
+var text = document.getElementsByClassName(text[website])[0].textContent;
 
 
 //get number of words in 
-var numWords = getText().split(" ").length;
+var numWords = text.split(" ").length;
 
 var textBox = document.getElementsByClassName('txtInput')[0];
 i = 0; //word counter
 
 //put words into textBox
 function addWord() {
-  word = text.split(" ")[i++] + " "; //split text into words
+  word = text.split(" ")[i++]; //split text into words
   textBox.value = word; //insert word
 }
 
@@ -23,12 +29,3 @@ window.onkeyup = function(e) {
     addWord();
   }
 };
-
-
-/*
-//hack test
-var timer = document.getElementsByClassName('timeDisplay')[0];
-timer.value = "60:00";
-*/
-//textBox.setAttribute('onkeyup', 'alert("Hello")'); //set textBox to react on keyup
-//textBox.removeAttribute('onkeyup');
