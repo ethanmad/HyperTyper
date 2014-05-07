ebdb75c7f009501cd9100567ac4a749d6f5c0836
Ethan Madison <ethanmadison@gmail.com>
2014-05-07 13:39:36 -0700

    Added ability to read from classes and Ids

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-//index 0 = TypeRacer, 1 = typrX
+//index 0 = TypeRacer, 1 = typrX, 2=10fastfingers
 var textOptions = ['nonHideableWords unselectable', 'cw-QuotePanel-textToTypePanel', 'textBox'];
-var textBoxOptions = ['txtInput', 'cw-TypedTextBox race-go', 'form-control'];   
+var inputBoxOptions = ['txtInput', 'cw-TypedinputBox race-go', 'form-control'];   
 
 var website = -1;
+var classOrId = -1;
 for(w = 0; w < textOptions.length; w++) {
   if(!(document.getElementsByClassName(textOptions[w])[0] == null)) {
     website = w;
+    classOrId = 0;
+    break;
+  }
+  if(!(document.getElementById(textOptions[w] == null))) {
+    website = w;
+    classOrId = 1;
     break;
   }
 }
 
-if(website > -1) {
+if(website > -1 && classOrId > -1) {
   //get passage from HTML
-  var text = document.getElementsByClassName(textOptions[website])[0].textContent;
-  
+  var text;
+  if(classOrId = 0)
+    text = document.getElementsByClassName(textOptions[website])[0].textContent;
+  else if (classOrId = -1)
+    text = document.getElementById(textOptions[website]).textContent;
   
   //get number of words in 
   var numWords = text.split(" ").length;
   
-  var textBox = document.getElementsByClassName(textBoxOptions[website])[0];
+  //find box to put words into
+  var input = document.getElementsByClassName(inputBoxOptions[website])[0];
   i = 0; //word counter
   
-  //put words into textBox
+  //put words into inputBox
   function addWord() {
     if(i>=numWords) return;
     word = text.split(" ")[i++]; //split text into words
-    textBox.value = word; //insert word
+    inputBox.value = word; //insert word
   }
   
   addWord();
