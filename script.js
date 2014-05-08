6887b4f1bc1a0418d010995587f51ae0766fd869
ethan <ethanmadison@gmail.com>
2014-05-07 18:06:12 -0700

    broke something

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-//index 0 = TypeRacer, 1 = typrX, 2=10fastfingers
-var textOptions = ['nonHideableWords unselectable', 'cw-QuotePanel-textToTypePanel', 'row1'];
-var inputBoxOptions = ['txtInput', 'cw-TypedinputBox race-go', 'form-control'];   
+//index 0 = TypeRacer, 1 = typrX, 2 = 10FastFingers, 3 = Typing Test
+var textOptions = ['nonHideableWords unselectable', 'cw-QuotePanel-textToTypePanel', 'row1', 'practiceText'];
+var inputBoxOptions = ['txtInput', 'cw-TypedinputBox race-go', 'form-control', 'tentry'];   
 
 var website = -1;
 var classOrId = -1;
+
 for(w = 0; w < textOptions.length; w++) {
   if(!(document.getElementsByClassName(textOptions[w])[0] == null)) {
     website = w;
     classOrId = 0;
     break;
-  }
-  else if(!(document.getElementById(textOptions[w]) == null)) {
+  } else if(!(document.getElementById(textOptions[w]) == null)) {
     website = w;
     classOrId = 1;
     break;
@@ -20,21 +20,27 @@ for(w = 0; w < textOptions.length; w++) {
 if(website > -1 && classOrId > -1) {
   //get passage from HTML
   var text;
-  if(classOrId = 0)
+  if(classOrId = 0) {
     text = document.getElementsByClassName(textOptions[website])[0].textContent;
-  else if (classOrId = -1)
+  } else if (classOrId = 1) {
     text = document.getElementById(textOptions[website]).textContent;
-  
-  //get number of words in 
+  }
+
+  //get number of words in text
   var numWords = text.split(" ").length;
   
-  //find box to put words into
-  var inputBox = document.getElementsByClassName(inputBoxOptions[website])[0];
+  //find inputBox to put words into
+  var inputBox;
+  if(!(document.getElementsByClassName(inputBoxOptions[website])[0] == null))
+    inputBox = document.getElementsByClassName(inputBoxOptions[website])[0];
+  else if(!(document.getElementsById(inputBoxOptions[website])))
+    inputBox = document.getElementsById(inputBoxOptions[website]);
+
   i = 0; //word counter
   
   //put words into inputBox
   function addWord() {
-    if(i>=numWords) return;
+    if(i >= numWords)   return;
     word = text.split(" ")[i++]; //split text into words
     inputBox.value = word; //insert word
   }
